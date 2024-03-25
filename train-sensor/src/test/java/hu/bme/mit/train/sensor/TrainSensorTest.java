13cb7672a15bb85cacf17b57ec37d3cb275f4a88
SolyomBalint <balint.solyom01@gmail.com>
2024-03-25 17:37:14 +0100

    import fix

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,8 +3,9 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 
 import static org.mockito.Mockito.*;
 
