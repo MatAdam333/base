8ab5e91764eed44e401effe8a9835cb6ec442cb2
Adam <matkovics.adam@gmail.com>
2024-03-25 16:11:31 +0100

    Fixed include

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,6 +3,7 @@ package hu.bme.mit.train.controller;
 import hu.bme.mit.train.interfaces.TrainController;
 
 import java.util.Timer;
+import java.util.Timer.TimerTask;
 
 public class TrainControllerImpl implements TrainController {
 
